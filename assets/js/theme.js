79061c956df29c35c80b71272402e0c5c8bb0a19
Gundars <gundarens@gmail.com>
2023-09-06 19:41:29 +0300

    Play button upgrade

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,3 +1,4 @@
+//Function for icon that appears when scrolling and when clicked takes to top
 
 window.onscroll = function() {
     scrollFunction();
@@ -21,31 +22,27 @@ function scrollToTop() {
     document.documentElement.scrollTop = 0;
 }
 
+//Function for button sizing in Video container 
 
 const parentElement = document.querySelector('.video-container');
 const playButton = document.querySelector('.play-button');
-const icon = document.querySelector('.fa-play'); // Select the icon element
+const icon = document.querySelector('.fa-play'); 
 
-// Function to update the button's dimensions based on the parent's width
 function updateButtonSize() {
   const parentWidth = parentElement.offsetWidth;
-  const buttonWidthPercentage = 15; // Adjust this as needed
+  const buttonWidthPercentage = 15; 
 
   const buttonWidth = (parentWidth * buttonWidthPercentage) / 100;
-  const buttonHeight = buttonWidth; // Make it square
+  const buttonHeight = buttonWidth; 
 
   playButton.style.width = `${buttonWidth}px`;
   playButton.style.height = `${buttonHeight}px`;
 
-  // Calculate the maximum allowed icon font size based on button size
   const maxIconFontSize = buttonWidth * 3.2;
 
-  // Set the icon font size, but limit it to the maximum allowed size
   icon.style.fontSize = `${Math.min(maxIconFontSize, 300)}%`;
 }
 
-// Initial call to set the button's size and icon size
 updateButtonSize();
 
-// Add an event listener to update the button's size and icon size when the window is resized
 window.addEventListener('resize', updateButtonSize);
