25e425c4ef43e197eca61ac1bd1c78ea266e1745
Gundars <gundarens@gmail.com>
2023-09-26 10:47:42 +0300

    Made manu for small screens

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -22,3 +22,29 @@ function scrollToTop() {
     document.documentElement.scrollTop = 0;
 }
 
+
+// Get the menu and close icons by their IDs
+const menuIcon = document.getElementById("menu-icon");
+const closeIcon = document.getElementById("close-icon");
+const headerNavbarPopup = document.querySelector(".header-navbar-popup");
+
+// Add a click event listener to the menu icon
+menuIcon.addEventListener("click", function () {
+    // Toggle the visibility of the popup menu
+    headerNavbarPopup.style.display = "flex";
+
+    // Hide the menu icon and show the close icon
+    menuIcon.style.display = "none";
+    closeIcon.style.display = "flex";
+});
+
+// Add a click event listener to the close icon
+closeIcon.addEventListener("click", function () {
+    // Toggle the visibility of the popup menu
+    headerNavbarPopup.style.display = "none";
+
+    // Hide the close icon and show the menu icon
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "flex";
+});
+
