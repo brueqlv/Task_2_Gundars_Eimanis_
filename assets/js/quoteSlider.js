00c1807d1c213ad8cbecbcde3d84138ec1cdffef
Gundars <gundarens@gmail.com>
2023-09-29 11:15:22 +0300

    fixed quote slider

--- assets/js/quoteSlider.js
+++ assets/js/quoteSlider.js
@@ -6,25 +6,33 @@ const forwardButton = document.querySelector('.forward');
 let currentIndex = 0;
 
 function showNextQuote() {
-    if (currentIndex < quotes.length - 1) {
-        currentIndex++;
-        updateQuoteDisplay();
-    }
+  if (currentIndex < quotes.length - 1) {
+    currentIndex++;
+    updateQuoteDisplay();
+  }
 }
 
 function showPreviousQuote() {
-    if (currentIndex > 0) {
-        currentIndex--;
-        updateQuoteDisplay();
-    }
+  if (currentIndex > 0) {
+    currentIndex--;
+    updateQuoteDisplay();
+  }
 }
 
 function updateQuoteDisplay() {
-    const quoteWidth = quotes[0].offsetWidth;
-    const translateX = -currentIndex * quoteWidth;
-    quoteContainer.style.transform = `translateX(${translateX}px)`;
-    backButton.disabled = currentIndex === 0;
-    forwardButton.disabled = currentIndex === quotes.length - 1;
+  const quoteWidthPercentage = 100 / quotes.length; // Calculate flex percentage
+
+  quoteContainer.style.width = `${quotes.length * 100}%`; // Set the container width
+
+  // Set flex value for each quote
+  quotes.forEach((quote) => {
+    quote.style.flex = `0 0 ${quoteWidthPercentage}%`;
+  });
+
+  const translateX = -currentIndex * quoteWidthPercentage;
+  quoteContainer.style.transform = `translateX(${translateX}%)`;
+  backButton.disabled = currentIndex === 0;
+  forwardButton.disabled = currentIndex === quotes.length - 1;
 }
 
-updateQuoteDisplay(); 
+updateQuoteDisplay();
